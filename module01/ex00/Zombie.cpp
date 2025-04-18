400356d2e4903bdf7ee9fea6500f492b5159ade1
Seilan <seilanmoore@gmail.com>
2025-04-18 15:35:36 +0200

    include guard added

--- module01/ex00/Zombie.cpp
+++ module01/ex00/Zombie.cpp
@@ -6,18 +6,26 @@
 /*   By: smoore-a <smoore-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 13:13:00 by smoore-a          #+#    #+#             */
-/*   Updated: 2025/04/18 14:00:21 by smoore-a         ###   ########.fr       */
+/*   Updated: 2025/04/18 15:33:36 by smoore-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+Zombie::Zombie(void) : name_("Paco")
+{
+}
+
+Zombie::Zombie(std::string const &name) : name_(name)
+{
+}
 
 Zombie::~Zombie(void)
 {
 	std::cout << this->name_ << ": has been destroyed" << std::endl;
 }
 
-void Zombie::announce(void)
+void Zombie::announce(void) const
 {
 	std::cout << this->name_ << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
--- module01/ex00/Zombie.hpp
+++ module01/ex00/Zombie.hpp
@@ -6,19 +6,22 @@
 /*   By: smoore-a <smoore-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 13:06:15 by smoore-a          #+#    #+#             */
-/*   Updated: 2025/04/18 13:28:50 by smoore-a         ###   ########.fr       */
+/*   Updated: 2025/04/18 15:33:16 by smoore-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
 
 #include <iostream>
 
 class Zombie
 {
 public:
-	Zombie(void) : name_("Paco") {};
-	Zombie(std::string const &name) : name_(name) {};
+	Zombie(void);
+	Zombie(std::string const &name);
 	~Zombie(void);
-	void announce(void);
+	void announce(void) const;
 
 private:
 	std::string name_;
@@ -26,3 +29,5 @@ private:
 
 Zombie *newZombie(std::string const &name);
 void randomChump(std::string const &name);
+
+#endif
