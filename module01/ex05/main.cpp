b9d1aa101b378c9bfc2810a716ac4c2054e30dc6
Seilan <seilanmoore@gmail.com>
2025-04-21 16:57:45 +0200

    added empty parameter complain call

--- module01/ex05/main.cpp
+++ module01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: smoore-a <smoore-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 11:53:23 by smoore-a          #+#    #+#             */
-/*   Updated: 2025/04/20 19:11:31 by smoore-a         ###   ########.fr       */
+/*   Updated: 2025/04/21 16:56:53 by smoore-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int main(void)
 {
 	Harl harl;
 
+	harl.complain("");
 	harl.complain("DEBUG");
 	harl.complain("INFO");
 	harl.complain("WARNING");
